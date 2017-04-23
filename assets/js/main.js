c9130aa0964076687510394e725eac87e8ad500e
selikem <selikem@gmail.com>
2017-04-23 10:34:20 -0700

    added smooth scrolling and alert box with jquery

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,30 @@
 $(function(){
 
   // smooth scrolling
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 500);
+        return false;
+      }
+    }
+  });
 
   //flickity
 
   //alert box behaviour
+  $('#signup-form').on('submit', function(event) {
+    event.preventDefault();
+    if ($('#signup-input').val() === '') {
+        alert('Please enter a valid email address')
+        } else {
+        alert('Thanks for subscribing!')
+        }
+  });
 
 
 })
